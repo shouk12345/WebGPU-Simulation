627927e2851896d9f886da688f21b96c6e1569c9
shouk12345 <wai1205@naver.com>
2026-05-12 01:54:57 +0900

    add : GitHub pages deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import wgsl from 'vite-plugin-wgsl';
 
 export default defineConfig({
+  base:'/WebGPU-Simulation/',
   plugins: [wgsl({
     include: [/\.wgsl$/]
   })],
